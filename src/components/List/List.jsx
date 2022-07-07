a49c1e8294ed4dc9b4891decb56c4e0986de629e
Vani Singh <vanisingh6612@gmail.com>
2022-07-07 12:34:37 +0530

    map

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect, createRef } from 'react';
 import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
 
 import PlaceDetails from '../PlaceDetails/PlaceDetails';
-
 import useStyles from './styles';
 
 const List = ({ places, childClicked, isLoading, type, setType, rating, setRating }) => {
@@ -17,7 +16,7 @@ const List = ({ places, childClicked, isLoading, type, setType, rating, setRatin
     return (
         <div className={classes.container}>
             <Typography variant="h4">
-                Restaurants, Hotels & Attractions around you
+                Food & Dining around you
             </Typography>
             {isLoading ? (
                 <div className={classes.loading}>
